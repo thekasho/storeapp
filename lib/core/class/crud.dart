0da94f4ddf424fcbc38d4c3d9c90c996651b5638
thekasho <kasho.mobi1@gmail.com>
2023-06-18 19:30:05 +0300

    add crud class to handle post requests

---
+++ lib/core/class/crud.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+import 'package:dartz/dartz.dart';
+import 'package:http/http.dart' as http;
+
+import 'package:storefull/core/class/statusrequest.dart';
+import 'package:storefull/core/functions/checknetwork.dart';
+
+class Crud {
+  Future<Either<StatusRequest, Map>> postData(String likurl, Map data) async {
+    try {
+      if (await checkNetwork()) {
+        var response = await http.post(Uri.parse(likurl), body: data);
+        if (response.statusCode == 200 || response.statusCode == 201) {
+          Map responsebody = jsonDecode(response.body);
+          return Right(responsebody);
+        } else {
+          return const Left(StatusRequest.serverFailure);
+        }
+      } else {
+        return const Left(StatusRequest.offlineFailure);
+      }
+    } on Exception {
+      return const Left(StatusRequest.serverFailure);
+    }
+  }
+}
