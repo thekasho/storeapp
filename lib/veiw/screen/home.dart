21edfd5f08d514a7d6763dd3872c8d116f4a5732
thekasho <kasho.mobi1@gmail.com>
2023-06-21 21:20:33 +0300

    add search bar and notification icon to home page

--- lib/veiw/screen/home.dart
+++ lib/veiw/screen/home.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:storefull/controller/home_controller.dart';
 
-import '../../core/constant/colors.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,21 +10,52 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     HomeControllerImp controller = Get.put(HomeControllerImp());
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: AppColor.backgroundcolor,
-        elevation: 0.0,
-        centerTitle: true,
-        title: Text("Home",
-            style: Theme.of(context)
-                .textTheme
-                .displayLarge!
-                .copyWith(color: AppColor.grey)),
-      ),
-      body: Center(
-        child: Text(
-          "${controller.username} ${controller.id}",
-          style: TextStyle(fontSize: 30.0),
-        ),
+      body: ListView(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(top: 10.0),
+            padding: const EdgeInsetsDirectional.symmetric(horizontal: 15.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(
+                        Icons.search,
+                        size: 30.0,
+                        color: Colors.grey[600],
+                      ),
+                      hintText: "Find Product..",
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      filled: true,
+                      fillColor: Colors.grey[200],
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 10.0),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20.0),
+                    color: Colors.grey[200],
+                  ),
+                  padding: const EdgeInsetsDirectional.symmetric(vertical: 5.0),
+                  width: 60.0,
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.notifications_active_outlined,
+                      size: 30.0,
+                      color: Colors.grey[600],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
